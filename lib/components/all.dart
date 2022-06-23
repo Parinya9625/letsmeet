26a995f48b6d99c1dd23ad4fd5106489040889c1
Parinya9625 <parinya9625@hotmail.com>
2022-06-23 21:20:34 +0700

    Update test components page

--- lib/components/all.dart
+++ lib/components/all.dart
@@ -1,12 +1,22 @@
 // ----- Components -----
+export 'chat_bubble.dart';
+export 'chat_group_card.dart';
+export 'checkbox_tile.dart';
 export 'continue_with_google.dart';
+export 'event_card_admin.dart';
+export 'event_card_admin_report.dart';
 export 'image_cover_picker.dart';
 export 'interest_category_selector.dart';
 export 'no_connection_banner.dart';
 export 'no_event_banner.dart';
 export 'no_search_result_banner.dart';
+export 'review_user_card.dart';
 export 'search_event_card.dart';
+export 'search_user_card.dart';
+export 'shimmer.dart';
 export 'textfield_extension.dart';
 // ----- Controllers -----
+export 'controllers/checkbox_tile_controller.dart';
 export 'controllers/image_picker_controller.dart';
 export 'controllers/interest_category_controller.dart';
+export 'controllers/review_user_controller.dart';
