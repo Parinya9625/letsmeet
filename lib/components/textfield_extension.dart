d4eae42d4a642f500eee8b86c284704ddf9203a2
Parinya9625 <parinya9625@hotmail.com>
2022-07-12 20:07:39 +0700

    Add support for TextFormField

--- lib/components/textfield_extension.dart
+++ lib/components/textfield_extension.dart
@@ -14,3 +14,16 @@ extension TextFieldExtension on TextField {
     );
   }
 }
+
+extension TextFormFieldExtension on TextFormField {
+  Material withElevation() {
+    return Material(
+      color: Colors.white,
+      elevation: 1.0,
+      borderRadius: const BorderRadius.all(
+        Radius.circular(16),
+      ),
+      child: this,
+    );
+  }
+}
