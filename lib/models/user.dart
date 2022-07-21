074087d6330d95cf3c416ab9839940a512d1200a
Parinya9625 <parinya9625@hotmail.com>
2022-07-21 11:48:21 +0700

    Update rating in user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -4,6 +4,57 @@ import 'package:letsmeet/models/category.dart';
 import 'package:letsmeet/models/event.dart';
 import 'package:letsmeet/models/role.dart';
 
+class UserRating {
+  final int r1;
+  final int r2;
+  final int r3;
+  final int r4;
+  final int r5;
+
+  UserRating({
+    this.r1 = 0,
+    this.r2 = 0,
+    this.r3 = 0,
+    this.r4 = 0,
+    this.r5 = 0,
+  });
+
+  factory UserRating.fromMap(Map<String, int> value) {
+    return UserRating(
+      r1: value["1"]!,
+      r2: value["2"]!,
+      r3: value["3"]!,
+      r4: value["4"]!,
+      r5: value["5"]!,
+    );
+  }
+
+  Map<String, int> toMap() {
+    return {
+      "1": r1,
+      "2": r2,
+      "3": r3,
+      "4": r4,
+      "5": r5,
+    };
+  }
+
+  int amount() {
+    return r1 + r2 + r3 + r4 + r5;
+  }
+
+  int total() {
+    return r1 + (r2 * 2) + (r3 * 3) + (r4 * 4) + (r5 * 5);
+  }
+
+  double average() {
+    if (amount() == 0) {
+      return 0;
+    }
+    return total() / amount();
+  }
+}
+
 class User {
   final String? id;
   final String bio;
@@ -12,7 +63,7 @@ class User {
   final List<DocumentReference> favCategory;
   final String image;
   final String name;
-  final List<int> rating;
+  final UserRating rating;
   final List<DocumentReference> recentView;
   final DocumentReference role;
   final String surname;
@@ -42,7 +93,7 @@ class User {
         bio = "",
         createdTime = createdTime ?? DateTime.now(),
         favCategory = [],
-        rating = [],
+        rating = UserRating(),
         recentView = [],
         isFinishSetup = false,
         role = FirebaseFirestore.instance
@@ -58,7 +109,7 @@ class User {
   })  : bio = "",
         createdTime = createdTime ?? DateTime.now(),
         favCategory = [],
-        rating = [],
+        rating = UserRating(),
         recentView = [],
         isFinishSetup = false,
         role = FirebaseFirestore.instance
@@ -84,7 +135,7 @@ class User {
       favCategory: List<DocumentReference>.from(data["favCategory"]),
       image: data["image"],
       name: data["name"],
-      rating: List<int>.from(data["rating"]),
+      rating: UserRating.fromMap(Map<String, int>.from(data["rating"])),
       recentView: List<DocumentReference>.from(data["recentView"]),
       role: data["role"],
       surname: data["surname"],
@@ -104,7 +155,7 @@ class User {
       "favCategory": favCategory,
       "image": image,
       "name": name,
-      "rating": rating,
+      "rating": rating.toMap(),
       "recentView": recentView,
       "role": role,
       "surname": surname,
