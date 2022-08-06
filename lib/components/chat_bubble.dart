877d790743550ea04ccd9de21423817f9d959a56
Parinya9625 <parinya9625@hotmail.com>
2022-08-06 14:16:26 +0700

    Remove future builder (cause lag when load)

--- lib/components/chat_bubble.dart
+++ lib/components/chat_bubble.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:letsmeet/components/shimmer.dart';
@@ -6,6 +7,7 @@ import 'package:letsmeet/models/chat.dart';
 import 'package:letsmeet/models/user.dart';
 
 class ChatBubble extends StatefulWidget {
+  final User? by;
   final Chat chat;
   final bool isSender;
   final bool isContinue;
@@ -14,6 +16,7 @@ class ChatBubble extends StatefulWidget {
 
   const ChatBubble({
     Key? key,
+    required this.by,
     required this.chat,
     this.isSender = false,
     this.isContinue = false,
@@ -60,24 +63,25 @@ class _ChatBubbleState extends State<ChatBubble> {
         children: [
           // profile
           if (widget.isSender) ...{
-            if (widget.isContinue) ...{
-              const SizedBox(
-                width: 40,
-                height: 40,
-              ),
-            } else ...{
-              ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: GestureDetector(
-                  onTap: widget.onTapProfile,
-                  child: CachedNetworkImage(
-                    imageUrl: user.image,
-                    width: 40,
-                    height: 40,
-                  ),
-                ),
-              ),
-            },
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: widget.isContinue
+                  ? const SizedBox(
+                      width: 40,
+                      height: 40,
+                    )
+                  : ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: GestureDetector(
+                        onTap: widget.onTapProfile,
+                        child: CachedNetworkImage(
+                          imageUrl: user.image,
+                          width: 40,
+                          height: 40,
+                        ),
+                      ),
+                    ),
+            ),
           },
           // chat
           Padding(
@@ -173,6 +177,7 @@ class _ChatBubbleState extends State<ChatBubble> {
         Container(
           width: 40,
           height: 40,
+          padding: const EdgeInsets.only(left: 8.0),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(20),
             color: widget.isContinue ? null : Colors.black,
@@ -197,60 +202,158 @@ class _ChatBubbleState extends State<ChatBubble> {
       builder: (context, constraints) {
         // Alert
         if (widget.chat.isAlert) {
-          return ConstrainedBox(
-            constraints: BoxConstraints(maxWidth: constraints.maxWidth / 1.25),
-            child: Card(
-              color: Theme.of(context).cardColor.withOpacity(0.75),
-              elevation: 0,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 16,
-                ),
-                child: Column(
-                  children: [
-                    if (widget.showTime) ...{
-                      chatTime(),
-                    },
-                    const SizedBox(height: 4),
-                    Text(
-                      widget.chat.text.toString(),
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.bodyText1,
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ConstrainedBox(
+                constraints:
+                    BoxConstraints(maxWidth: constraints.maxWidth / 1.25),
+                child: Card(
+                  color: Theme.of(context).cardColor.withOpacity(0.75),
+                  elevation: 0,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 16,
                     ),
-                  ],
+                    child: Column(
+                      children: [
+                        if (widget.showTime) ...{
+                          chatTime(),
+                        },
+                        const SizedBox(height: 4),
+                        Text(
+                          widget.chat.text.toString(),
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
-            ),
+            ],
           );
         }
         // Chat bubble
-        return FutureBuilder(
-          future: Future.wait([widget.chat.getBy]),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            return ShimmerLoading(
-              isLoading: !snapshot.hasData,
-              placeholder: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: widget.isSender
-                    ? MainAxisAlignment.start
-                    : MainAxisAlignment.end,
-                children: placeholder(constraints),
-              ),
-              builder: (BuildContext context) {
-                User user = snapshot.data[0];
-                return Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: widget.isSender
-                      ? MainAxisAlignment.start
-                      : MainAxisAlignment.end,
-                  children: chatBubble(constraints, user),
-                );
-              },
+        return ShimmerLoading(
+          isLoading: widget.by == null,
+          placeholder: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: widget.isSender
+                ? MainAxisAlignment.start
+                : MainAxisAlignment.end,
+            children: placeholder(constraints),
+          ),
+          builder: (BuildContext context) {
+            return Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: widget.isSender
+                  ? MainAxisAlignment.start
+                  : MainAxisAlignment.end,
+              children: chatBubble(constraints, widget.by!),
             );
           },
         );
       },
     );
+  }
+}
+
+class ProgressChatBubble extends StatefulWidget {
+  final Chat chat;
+
+  const ProgressChatBubble({
+    Key? key,
+    required this.chat,
+  }) : super(key: key);
+
+  @override
+  State<ProgressChatBubble> createState() => ProgressChatBubbleState();
+}
+
+class ProgressChatBubbleState extends State<ProgressChatBubble> {
+  Widget imageProgress(UploadTask task) {
+    return StreamBuilder<TaskSnapshot>(
+      stream: task.snapshotEvents,
+      builder: (context, snapshot) {
+        TaskSnapshot? taskSnapshot = snapshot.hasData ? snapshot.data : null;
+
+        double progress = taskSnapshot != null
+            ? taskSnapshot.bytesTransferred / taskSnapshot.totalBytes
+            : 0;
+
+        if (taskSnapshot?.state == TaskState.success) {
+          taskSnapshot!.ref.getDownloadURL();
+        }
+
+        // print("${event} ${progress} ${event?.bytesTransferred} ${event?.totalBytes}");
+
+        return Padding(
+          padding: const EdgeInsets.all(16),
+          child: CircularProgressIndicator(
+            value: progress != 0 ? progress : null,
+            color: Theme.of(context).cardColor,
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: constraints.maxWidth / 1.5),
+              child: Card(
+                color: Theme.of(context).primaryColor,
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // text chat
+                      if (widget.chat.text != null) ...{
+                        SelectableText(
+                          widget.chat.text!,
+                          style:
+                              Theme.of(context).textTheme.headline1!.copyWith(
+                                    color: Theme.of(context).cardColor,
+                                  ),
+                        ),
+                        const SizedBox(height: 6),
+                      },
+                      // image
+                      GridView.count(
+                        primary: false,
+                        shrinkWrap: true,
+                        crossAxisCount: 3,
+                        mainAxisSpacing: 8.0,
+                        crossAxisSpacing: 8.0,
+                        children: [
+                          const SizedBox(),
+                          Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: CircularProgressIndicator(
+                              color: Theme.of(context).cardColor,
+                            ),
+                          ),
+                          const SizedBox(),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      );
+    });
   }
 }
