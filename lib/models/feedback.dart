4b32f02d8eee7f45e2ea48506f85abedc7aef65d
Parinya9625 <parinya9625@hotmail.com>
2022-11-17 21:09:05 +0700

    Update : admin feedback page

--- lib/models/feedback.dart
+++ lib/models/feedback.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:letsmeet/models/user.dart';
 import 'package:letsmeet/services/firestore.dart';
 
 class Feedback {
@@ -20,6 +21,8 @@ class Feedback {
     DateTime? createdTime,
   })  : id = null,
         createdTime = createdTime ?? DateTime.now();
+
+  Future<User> get getBy async => User.fromFirestore(doc: await by.get());
 
   factory Feedback.fromFirestore({required DocumentSnapshot doc}) {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
